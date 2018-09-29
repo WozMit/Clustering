8d76c824021669dbc9fe26631f8f841eb6fee52d
WozMit <jaredleon.223@gmail.com>
2018-09-29 17:34:46 -0500

    Basic clustering framework working

--- clustering.c
+++ clustering.c
@@ -16,10 +16,6 @@ double J(double *distances){
 	return cost;
 }
 
-double AMI(int *clusters, int *labels){
-	return 0.12;
-}
-
 int main(int argc, char const *argv[]){
 	clock_t _start = clock();
 	// Read the initial data
@@ -72,7 +68,7 @@ int main(int argc, char const *argv[]){
 	centers[2][3] = 2.10679832;*/
 
 	// Generate clusters
-	int clusters[n];
+	int labels_pred[n];
 	double distances[n];
 	for(j=0; j<n; j++) distances[j] = 1<<30;
 	for(i=0; i<n; i++){
@@ -84,14 +80,13 @@ int main(int argc, char const *argv[]){
 			distance = sqrt(distance);
 			if(distance < distances[i]){
 				distances[i] = distance;
-				clusters[i] = j;
+				labels_pred[i] = j;
 			}
 		}
 	}
-	for(i=0; i<n; i++) printf("%d ", clusters[i]);
+	for(i=0; i<n; i++) printf("%d ", labels_pred[i]);
 	printf("\n");
 	printf("%lf\n", J(distances));
-	printf("Adjusted mutual information: %.3f %%\n", AMI(clusters, labels)*100);
 	printf("%30c Executed in %.3f s.",
 		32, (double)(clock() - _start)/CLOCKS_PER_SEC);
 	return 0;
