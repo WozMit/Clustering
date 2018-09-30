0adf891e5a077b0d0aceb7211d653e1c914c5a67
WozMit <jaredleon.223@gmail.com>
2018-09-29 22:05:39 -0500

    New datasets

--- datasets/get_datasets.py
+++ datasets/get_datasets.py
@@ -1,11 +1,16 @@
-from sklearn import datasets;
+import numpy as np;
 
-iris = datasets.load_breast_cancer();
-data = iris.data;
-target = iris.target;
-n, d, k = len(target), len(data[0]), len(set(target));
-print(n, d, k);
-for i in range(n):
-	for j in range(d):
-		print(data[i][j], "", end="");
-	print(target[i]);
+# Read the data
+ever = np.load('mnist.npz');
+data = ever['arr_0'];
+target_p = ever['arr_1'];
+target = np.zeros(70000);
+for i in range(70000):
+	for j in range(10):
+		if(target_p[i][j] == 1):
+			target[i] = j;
+print("70000 784 10");
+for i in range(70000):
+	for j in range(784):
+		print(int(data[i][j] * 255), "", end="");
+	print(int(target[i]));
