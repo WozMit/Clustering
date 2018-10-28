d34e41d634ed96899bf06bc918cb44f35cb41984
WozMit <jaredleon.223@gmail.com>
2018-10-28 10:12:43 -0500

    Some generated datasets

--- datasets/get_datasets.py
+++ datasets/get_datasets.py
@@ -1,16 +1,148 @@
 import numpy as np;
+import matplotlib.pyplot as plt;
 
-# Read the data
-ever = np.load('mnist.npz');
-data = ever['arr_0'];
-target_p = ever['arr_1'];
-target = np.zeros(70000);
-for i in range(70000):
-	for j in range(10):
-		if(target_p[i][j] == 1):
-			target[i] = j;
-print("70000 784 10");
-for i in range(70000):
-	for j in range(784):
-		print(int(data[i][j] * 255), "", end="");
-	print(int(target[i]));
+def generate_points_click():
+	fig, ax = plt.subplots()
+	plt.xlim((0, 5));
+	plt.ylim((0, 5));
+
+	x = [];
+	y = [];
+
+	fig, ax = plt.subplots()
+
+	plt.plot(0, 0, 'b.');
+	plt.plot(0, 5, 'b.');
+	plt.plot(5, 0, 'b.');
+	plt.plot(5, 5, 'b.');
+
+	def onclick(event):
+		ax.clear();
+		plt.plot(0, 0, 'b.');
+		plt.plot(0, 5, 'b.');
+		plt.plot(5, 0, 'b.');
+		plt.plot(5, 5, 'b.');
+		x.append(event.xdata);
+		y.append(event.ydata);
+		print(event.xdata, event.ydata);
+		plt.plot(x, y, 'b.');
+		plt.gcf().canvas.draw_idle();
+
+	cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+	plt.show();
+
+	data = np.zeros((len(x), 2));
+	for i in range(len(x)):
+		data[i] = x[i], y[i];
+
+	return data;
+
+def generate_on_distribution():
+	x = [];
+	y = [];
+
+	"""x1 = np.random.normal(-2, 1.3, 100);
+	y1 = np.random.normal(2, 1.3, 100);
+	for a, b in zip(x1, y1):
+		x.append(a);
+		y.append(b);
+
+	x2 = np.random.normal(0, 1.3, 100);
+	y2 = np.random.normal(2, 1.3, 100);
+	for a, b in zip(x2, y2):
+		x.append(a);
+		y.append(b);
+
+	x3 = np.random.normal(2, 1.3, 100);
+	y3 = np.random.normal(2, 1.3, 100);
+	for a, b in zip(x3, y3):
+		x.append(a);
+		y.append(b);
+
+	x4 = np.random.normal(-2, 1.3, 100);
+	y4 = np.random.normal(0, 1.3, 100);
+	for a, b in zip(x4, y4):
+		x.append(a);
+		y.append(b);
+
+	x5 = np.random.normal(0, 1.3, 100);
+	y5 = np.random.normal(0, 1.3, 100);
+	for a, b in zip(x5, y5):
+		x.append(a);
+		y.append(b);
+
+	x6 = np.random.normal(2, 1.3, 100);
+	y6 = np.random.normal(0, 1.3, 100);
+	for a, b in zip(x6, y6):
+		x.append(a);
+		y.append(b);
+
+	x7 = np.random.normal(-2, 1.3, 100);
+	y7 = np.random.normal(-2, 1.3, 100);
+	for a, b in zip(x7, y7):
+		x.append(a);
+		y.append(b);
+
+	x8 = np.random.normal(0, 1.3, 100);
+	y8 = np.random.normal(-2, 1.3, 100);
+	for a, b in zip(x8, y8):
+		x.append(a);
+		y.append(b);
+
+	x9 = np.random.normal(2, 1.3, 100);
+	y9 = np.random.normal(-2, 1.3, 100);
+	for a, b in zip(x9, y9):
+		x.append(a);
+		y.append(b);"""
+
+	x1 = np.random.gamma(2, 2, 100);
+	y1 = np.random.gamma(2, 2, 100);
+	for a, b in zip(x1, y1):
+		x.append(a);
+		y.append(b);
+
+	x2 = -np.random.gamma(2, 1, 100);
+	y2 = -np.random.gamma(2, 1, 100);
+	for a, b in zip(x2, y2):
+		x.append(a);
+		y.append(b);
+
+	x3 = np.random.gamma(2, 1.5, 100);
+	y3 = -np.random.gamma(2, 1.5, 100);
+	for a, b in zip(x3, y3):
+		x.append(a);
+		y.append(b);
+
+	plt.plot(x1, y1, 'b.');
+	plt.plot(x2, y2, 'r.');
+	plt.plot(x3, y3, 'g.');
+	plt.show();
+	data = np.zeros((300, 2));
+	for i in range(300):
+		data[i][0] = x[i];
+		data[i][1] = y[i];
+	return data;
+
+#data = generate_points_click();
+data = generate_on_distribution();
+#print(data);
+print(data.shape);
+#np.save('temp.npy', data);
+
+#data = np.load('temp.npy');
+
+#print(data);
+#print(data.shape);
+
+#plt.plot(data[:60, 0], data[:60, 1], 'b.');
+#plt.plot(data[60:, 0], data[60:, 1], 'r.');
+#plt.plot(data[100:, 0], data[100:, 1], 'g.');
+#plt.show();
+
+print(len(data), len(data[0]), 3);
+for i in range(len(data)):
+	if(i < 100): x = 0;
+	elif (i < 200): x = 1;
+	else: x = 2;
+	print(round(data[i, 0], 5), round(data[i, 1], 5), x);
